5450fa2ce48b1e2e202a33c0ce2d682838af1604
Vivian Bow <vivianbao1009@gmail.com>
2022-03-03 01:30:50 +0900

    Fixed position of initialize_on_precompile in config/application

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module Animu
   class Application < Rails::Application
     config.generators do |generate|
@@ -15,7 +14,7 @@ module Animu
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
